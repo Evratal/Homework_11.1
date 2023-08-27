088a22d66cd45eb51bb9437d7c4ec237965da8f1
Evgeniy Ratuev <evratal@yandex.ru>
2023-08-27 23:57:34 +0400

    Add second function in str_func.py and docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,28 @@
+"""
+First function
+"""
+
+
 def Up_string(old_string):
     return (str.upper(old_string))
+
+
+"""
+Add second function
+"""
+
+
+def new_up_strig(old_string):
+    new_string = ""
+    count = 1
+    new_string += str.upper(old_string[0])
+    while count + 1 <= len(old_string):
+        if old_string[count - 1] == " ":
+            new_string += str.upper(old_string[count])
+        else:
+            new_string += old_string[count]
+        count += 1
+    return new_string
+
+
+print(new_up_strig("hello world"))
